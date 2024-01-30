e00cdbc9b5e9b5d8568f61cbd3b7fd9e17f9b7b1
Kurkov11 <kurkovsw@icloud.com>
2024-01-30 16:31:24 +0100

    Add custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        ubuntuBold: ["Ubuntu-Bold", "sans-serif"],
+        ubuntuMedium: ["Ubuntu-Medium", "sans-serif"],
+        ubuntuRegular: ["Ubuntu-Regular", "sans-serif"]
+      },
+      colors: {
+        "neutral-700": 'hsl(231, 11%, 63%)',
+        "neutral-400": "hsl(229, 24%, 87%)",
+        "neutral-300": "hsl(217, 100%, 97%)",
+        "neutral-200": "hsl(231, 100%, 99%)",
+        "dark-blue": "hsl(213, 96%, 18%)",
+        "light-blue": "hsl(206, 94%, 87%)",
+        "purple-blue": "hsl(243, 100%, 62%)",
+        "pastel-blue": "hsl(228, 100%, 84%)",
+        "red": "hsl(354, 84%, 57%)"
+      }
     },
   },
   plugins: [],
