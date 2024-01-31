9d1f595c39fa62e2d32b68936efd982f8c1270b0
Kurkov11 <kurkovsw@icloud.com>
2024-01-31 15:41:54 +0100

    add click listener, move val variables inside Get current value onclick

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,13 @@ const nameIn = document.querySelector('#name');
 const emailIn = document.querySelector('#email');
 const phoneIn = document.querySelector('#phone');
 
-const nameVal = nameIn.value;
-const emailVal = emailIn.value;
-const phoneVal = phoneIn.value;
+
 
 const personalSubmit = document.querySelector('#personal-submit');
+
+personalSubmit.addEventListener("click", function(){
+    const nameVal = nameIn.value;
+    const emailVal = emailIn.value;
+    const phoneVal = phoneIn.value;
+
+});
