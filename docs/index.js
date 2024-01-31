004ea89f3f660cc8e7a40a81760fedaaa0bdd2da
Kurkov11 <kurkovsw@icloud.com>
2024-01-31 15:52:58 +0100

    Add e as a required argument to submit function

--- docs/index.js
+++ docs/index.js
@@ -6,7 +6,7 @@ const phoneIn = document.querySelector('#phone');
 
 const personalSubmit = document.querySelector('#personal-submit');
 
-personalSubmit.addEventListener("click", function(){
+personalSubmit.addEventListener("click", function(e){
     const nameVal = nameIn.value;
     const emailVal = emailIn.value;
     const phoneVal = phoneIn.value;
