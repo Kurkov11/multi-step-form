c58f3b7e1568683e17a6a068856f3119c5ee844d
Kurkov11 <kurkovsw@icloud.com>
2024-01-31 15:28:00 +0100

    Store inputs and their values in variables

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+const nameIn = document.querySelector('#name');
+const emailIn = document.querySelector('#email');
+const phoneIn = document.querySelector('#phone');
+
+const nameVal = nameIn.value;
+const emailVal = emailIn.value;
+const phoneVal = phoneIn.value;
