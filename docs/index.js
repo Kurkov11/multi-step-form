698a6697650ca5b3d4a4a8217178025a29251f41
Kurkov11 <kurkovsw@icloud.com>
2024-01-31 16:23:06 +0100

    add 'e' as function passed argument

--- docs/index.js
+++ docs/index.js
@@ -9,6 +9,6 @@ const phoneEmpty = document.querySelector('#phone__empty');
 const personalSubmit = document.querySelector('#personal-submit');
 
 const personalForm = document.querySelector('#personal-info');
-personalForm.addEventListener("submit", function(){
+personalForm.addEventListener("submit", function(e){
     
 });
