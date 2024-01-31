bfa836017bfccb7f5afa6a3f04c8fd30844c5f0f
Kurkov11 <kurkovsw@icloud.com>
2024-01-31 17:29:55 +0100

    show empty messages when value is empty

--- docs/index.js
+++ docs/index.js
@@ -3,7 +3,7 @@ const emailIn = document.querySelector('#email');
 const phoneIn = document.querySelector('#phone');
 
 const nameEmpty = document.querySelector('#name__empty');
-const emailEmpty = document.querySelector('#email');
+const emailEmpty = document.querySelector('#email__empty');
 const phoneEmpty = document.querySelector('#phone__empty');
 
 const personalSubmit = document.querySelector('#personal-submit');
@@ -13,18 +13,23 @@ personalForm.addEventListener("submit", function(e){
     const nameValue = nameIn.value;
     const emailValue = emailIn.value;
     const phoneValue = phoneIn.value;
-
-    let error = false;
+    
     if(nameValue === ""){
-        error = true;
+        e.preventDefault();
+        nameEmpty.style.display = 'inline' //default
+    }else{
+        nameEmpty.style.display = 'none'
     }
     if(emailValue === ""){
-        error = true;
+        e.preventDefault();
+        emailEmpty.style.display = 'inline' //default
+    }else{
+        emailEmpty.style.display = 'none'
     }
     if(phoneValue === ""){
-        error = true;
-    }
-    if(error){
         e.preventDefault();
+        phoneEmpty.style.display = 'inline' //default
+    }else{
+        phoneEmpty.style.display = 'none'
     }
 });
