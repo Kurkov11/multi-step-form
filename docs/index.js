73c5525946ced16e90b0be05426a9be072007796
Kurkov11 <kurkovsw@icloud.com>
2024-01-31 15:30:05 +0100

    give id to submit and store it as variable

--- docs/index.js
+++ docs/index.js
@@ -5,3 +5,5 @@ const phoneIn = document.querySelector('#phone');
 const nameVal = nameIn.value;
 const emailVal = emailIn.value;
 const phoneVal = phoneIn.value;
+
+const personalSubmit = document.querySelector('#personal-submit');
