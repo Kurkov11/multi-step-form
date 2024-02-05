d1ffa6822f1933543536c7daf29d1258f03be365
Kurkov11 <kurkovsw@icloud.com>
2024-02-05 14:08:57 +0100

    Add shadows to card

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,9 @@ module.exports = {
       backgroundImage: {
         "sidebar-mobile": "url('../assets/images/bg-sidebar-mobile.svg')",
       },
+      boxShadow: {
+        "card-shadow": "rgba(0, 0, 0, 0.1) 0px 4px 12px;",
+      },
     },
   },
   plugins: [],
